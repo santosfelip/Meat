4b5f3847a7d7923b582d3d6db666a69e829d2705
santosfelip <nascimento.fe14@gmail.com>
2020-10-14 09:12:06 -0400

    commit inicial

---
+++ src/app/restaurants/restaurants.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {RestaurantsService} from './restaurants.service'
+import {Restaurant} from './restaurant/restaurant.model'
+@Component({
+  selector: 'mt-restaurants',
+  templateUrl: './restaurants.component.html',
+})
+export class RestaurantsComponent implements OnInit {
+  restaurants: Restaurant[]
+  constructor(private restaurantsService: RestaurantsService) { }
+
+  ngOnInit() {
+    this.restaurantsService.restaurants().subscribe(restaurants =>
+        this.restaurants = restaurants
+      )
+  }
+
+}
